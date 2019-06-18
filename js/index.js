69e2889d1164dfed303d2589557bda375a1b8f7d
Andrew Mendez <andrew15722@yahoo.com>
2019-06-18 15:47:50 -0700

    got one animation. working on the others

--- js/index.js
+++ js/index.js
@@ -9,7 +9,24 @@ nav.addEventListener('mouseover', (event) => {
 	// event.target refers to the clicked <a> element
 	// This is different than event.currentTarget which would refer to the parent <nav> in this context
 	event.target.style.background = 'lightblue';
-	event.stopPropagation();
 	event.target.style.color = 'white';
 	event.target.style.borderRadius = '5px';
+	event.target.style.padding = '2px 2px';
+	console.log(nav);
+});
+
+nav.addEventListener('mouseout', (event) => {
+	// event.target refers to the clicked <a> element
+	// This is different than event.currentTarget which would refer to the parent <nav> in this context
+	event.target.style.background = 'white';
+	event.target.style.color = 'black';
+	event.target.style.borderRadius = '0';
+	event.target.style.padding = '0';
+	console.log(nav);
+});
+
+//
+const content = document.getElementByClassName('btn');
+content.addEventListener('keydown', (event) => {
+	console.log('hello');
 });
